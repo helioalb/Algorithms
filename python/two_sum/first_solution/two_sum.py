ee71d537662243adc97bfb9e702b8b4500c0af68
Helio Albano de Oliveira <helio.albano@mercadolivre.com>
2023-03-25 09:32:22 -0300

    Fix algorithm

--- python/two_sum/first_solution/two_sum.py
+++ python/two_sum/first_solution/two_sum.py
@@ -1,5 +1,8 @@
 def two_sum(numbers: list, target: int) -> list:
-    for i in range(len(numbers) - 1):
-        if numbers[i] + numbers[i + 1] == target:
-            return [numbers[i], numbers[i + 1]]    
+    numbers_size = len(numbers)
+
+    for i in range(numbers_size - 1):
+        for j in range(i + 1, numbers_size):
+            if numbers[i] + numbers[j] == target:
+                return [numbers[i], numbers[j]]
     return []
