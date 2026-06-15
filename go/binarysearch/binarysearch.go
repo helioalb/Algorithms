fc217be40cc96251351cef947b3ac152612e986c
Helio Albano de Oliveira <helioalb@gmail.com>
2026-06-15 09:03:37 -0300

    Rewrite binarysearch

--- go/binarysearch/binarysearch.go
+++ go/binarysearch/binarysearch.go
@@ -1,21 +1,22 @@
 package binarysearch
 
-import "fmt"
-
-func BinarySearch(arr []int, target int) (int, bool) {
+func BinarySearch(arr []int, target int) int, bool {
 	low := 0
 	high := len(arr)
 
 	for low < high {
 		mid := (low + high) / 2
-		fmt.Printf("low: %d, high: %d, mid: %d\n", low, high, mid)
-		if arr[mid] == target {
+
+		current := arr[mid]
+
+		if (current == target) {
 			return mid, true
-		} else if target > arr[mid] {
-			low = mid + 1
-		} else {
+		} else if (target < mid) {
 			high = mid - 1
+		} else {
+			low := mid + 1
 		}
 	}
+
 	return 0, false
 }
